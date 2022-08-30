73fb339fbc8d85fd5fe2834ec815eda402cf7039
Ariel Rios <ariel.rios@millicom.com>
2022-08-30 11:28:02 -0500

    feature: testing with mocking dependency autogenerate.

--- test/repository_test.dart
+++ test/repository_test.dart
@@ -1,21 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_unit_test_workshop_1/data/mocks/mock_remote_data_source.dart';
 import 'package:flutter_unit_test_workshop_1/data/repository.dart';
 import 'package:flutter_unit_test_workshop_1/domain/models/student.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'repository_test.mocks.dart';
+
+@GenerateMocks([RemoteDataSource])
 void main() {
   test(
-      "El método fetch() Devolverá una instancia de student cuando este es correcto",
-      () async {
-    Repository repo = Repository(MockRemoteDataSourceSuccess());
-    final Student result = await repo.fetch();
-    expect(result, isInstanceOf<Student>());
-  });
+    "El método fetch() Devolverá una instancia de student cuando este es correcto",
+    () async {
+      // Arrange
+      MockRemoteDataSource mockDS = MockRemoteDataSource();
+      Repository repo = Repository(mockDS);
+      when(mockDS.fetch())
+          .thenAnswer((_) => Future.value('{"name": "Alumno prueba"}'));
+      // Act
+      final Student result = await repo.fetch();
+      // Assert
+      expect(result, isInstanceOf<Student>());
+    },
+  );
 
   test(
-      "El método fetch() Devolverá throwException cuando se atrape alguna excepción",
-      () {
-    Repository repo = Repository(MockRemoteDataSourceFailure());
-    expect(() => repo.fetch(), throwsException);
-  });
+    "El método fetch() Devolverá throwException cuando se atrape alguna excepción",
+    () {
+      MockRemoteDataSource mockDS = MockRemoteDataSource();
+      Repository repo = Repository(mockDS);
+      when(mockDS.fetch()).thenThrow(const SocketException('Mock Exception'));
+      expect(() => repo.fetch(), throwsException);
+    },
+  );
 }
