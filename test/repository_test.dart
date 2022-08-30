88ed693d65329bba85453e7ce013a2471fa8c9b0
Ariel Rios <ariel.rios@millicom.com>
2022-08-30 11:40:38 -0500

    feature: test with mocktail.

--- test/repository_test.dart
+++ test/repository_test.dart
@@ -3,12 +3,10 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_unit_test_workshop_1/data/repository.dart';
 import 'package:flutter_unit_test_workshop_1/domain/models/student.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'repository_test.mocks.dart';
+class MockRemoteDataSource extends Mock implements RemoteDataSource {}
 
-@GenerateMocks([RemoteDataSource])
 void main() {
   test(
     "El método fetch() Devolverá una instancia de student cuando este es correcto",
@@ -16,7 +14,7 @@ void main() {
       // Arrange
       MockRemoteDataSource mockDS = MockRemoteDataSource();
       Repository repo = Repository(mockDS);
-      when(mockDS.fetch())
+      when(() => mockDS.fetch())
           .thenAnswer((_) => Future.value('{"name": "Alumno prueba"}'));
       // Act
       final Student result = await repo.fetch();
@@ -30,7 +28,8 @@ void main() {
     () {
       MockRemoteDataSource mockDS = MockRemoteDataSource();
       Repository repo = Repository(mockDS);
-      when(mockDS.fetch()).thenThrow(const SocketException('Mock Exception'));
+      when(() => mockDS.fetch())
+          .thenThrow(const SocketException('Mock Exception'));
       expect(() => repo.fetch(), throwsException);
     },
   );
