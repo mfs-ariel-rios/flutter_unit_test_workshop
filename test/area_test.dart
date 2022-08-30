75378ca6dd1e2587893c0f401cb6711d4721b279
Ariel Rios <ariel.rios@millicom.com>
2022-08-30 08:10:54 -0500

    feature: test with setupAll

--- test/area_test.dart
+++ test/area_test.dart
@@ -2,14 +2,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_unit_test_workshop_1/domain/models/area.dart';
 
 void main() {
+  late Area area;
+
+  setUpAll(() {
+    area = Area();
+  });
+
   group('Area del circulo', () {
     test('Area del circulo con area 1 debe ser 3.141592', () {
       // Arrange
-      Area area = Area();
+
       // Act
       double result = area.circle(1);
       // Assert
       expect(result, 3.141592);
+    });
+
+    test('Area del circulo con area 10 debe ser 314.1592', () {
+      // Arrange
+
+      // Act
+      double result = area.circle(10);
+      // Assert
+      expect(result, 314.1592);
     });
 
     test('Area.pi debería ser 3.141592', () {
